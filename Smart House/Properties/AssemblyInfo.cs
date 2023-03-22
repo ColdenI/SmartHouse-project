29467efec345974452cec7367f68961a6f318772
Colden I <84040711+ColdenI@users.noreply.github.com>
2023-03-22 06:41:46 +0300

    Add files via upload

---
+++ Smart House/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("Smart House")]
+[assembly: AssemblyDescription("Программа позволяет управлять модулями системы Smart House.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Colden I Production")]
+[assembly: AssemblyProduct("Smart House")]
+[assembly: AssemblyCopyright("Colden I Production  ©  2022")]
+[assembly: AssemblyTrademark("Colden I Program")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("42cfaba6-d92e-4fff-86a7-f92a37dde4fb")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.1.0.1")]
+[assembly: AssemblyFileVersion("1.1.0.1")]
